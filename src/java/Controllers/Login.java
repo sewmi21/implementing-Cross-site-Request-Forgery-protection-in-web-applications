2edda712f342a835e07ffe0a1056a63bf89c21b3
sewmi21 <seud721@gmail.com>
2018-09-01 12:05:09 +0530

    Login Interface

---
+++ src/java/Controllers/Login.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class Login {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String User, Pass;
+        User=input.nextLine();
+        Pass=input.nextLine();
+        
+        if((User.equals("Sewmi"))&& (Pass.equals("sewmi123"))){
+            
+        }
+    }
+}
